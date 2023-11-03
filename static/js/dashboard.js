bb68a924dbe816e1872f8be6acb48adff796b002
Cristián Felipe Aponte León <apsistemas1@CERAMIGRESSA.LOCAL>
2023-11-03 09:45:19 -0500

    se usan nuevos estilos y se cambia el login para una mejor visualizacion

---
+++ static/js/dashboard.js
@@ -0,0 +1,93 @@
+window.addEventListener("load", function () {
+
+    var reportContainer = document.getElementById("report-container");
+    powerbi.bootstrap(reportContainer, { type: "report" });
+
+    var models = window["powerbi-client"].models;
+    var reportLoadConfig = {
+        type: "report",
+        tokenType: models.TokenType.Embed,
+    };
+
+    fetch('/getembedinfo', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        //   body: JSON.stringify({ username, password }),
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            reportLoadConfig.accessToken = data.accessToken;
+
+            // You can embed different reports as per your need
+            reportLoadConfig.embedUrl = data.reportConfig[0].embedUrl;
+
+            // Use the token expiry to regenerate Embed token for seamless end user experience
+            // Refer https://aka.ms/RefreshEmbedToken
+            tokenExpiry = data.tokenExpiry;
+
+            // Embed Power BI report when Access token and Embed URL are available
+            var report = powerbi.embed(reportContainer, reportLoadConfig);
+
+        });
+
+    // Peticion para obtener los CO
+    const token = localStorage.getItem('access_token');
+    const user_id = localStorage.getItem('user_id');
+
+    fetch('/get-co-list', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token, user_id }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Obtén el elemento select por su ID
+            const select = document.getElementById("co");
+
+            // Recorre el arreglo de opciones con forEach y crea elementos option
+            data.forEach(function (opcion) {
+                const option = document.createElement("option");
+                option.value = opcion.f_id;  // El valor de la opción
+                option.text = opcion.f_descripcion;   // El texto visible de la opción
+                select.appendChild(option);  // Agrega la opción al select
+            });
+
+        });
+});
+
+// Obtener el elemento select
+const select = document.getElementById("co");
+
+// Agregar un listener onchange al select
+select.addEventListener("change", function () {
+    // Obtener el valor seleccionado
+    const filterValue = select.value;
+    const reportContainer = document.getElementById('report-container');
+    const report = powerbi.get(reportContainer);
+
+    report.setFilters([{
+        $schema: "http://powerbi.com/product/schema#basic",
+        target: {
+            table: 'centros_operaciones',
+            column: 'centro_operaciones'
+        },
+        operator: 'In',
+        values: [filterValue]
+    }]);
+
+});
+
+// Obtener el elemento select
+const logout = document.getElementById("logout");
+logout.addEventListener('click', function () {
+    // Eliminar un valor de localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('user_id');
+    localStorage.removeItem('user_cedula');
+    window.location.href = "/";
+});
--- static/js/login.js
+++ static/js/login.js
@@ -1,41 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const loginForm = document.getElementById('login-form');
-//     const message = document.getElementById('message');
-
-//     loginForm.addEventListener('submit', function (e) {
-//         e.preventDefault();
-//         message.classList.add('hidden');
-
-//         const usuario = document.getElementById('usuario').value;
-//         const password = document.getElementById('password').value;
-
-//         // Hacer una solicitud a la API con usuario y contraseña
-//         fetch('https://olimpo.gentesplendor.com/api-trafico/login', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({ usuario, password }),
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data.success) {
-//                 // El inicio de sesión fue exitoso
-//                 window.location.href = 'index.html'; // Redirigir a la página de inicio
-//             } else {
-//                 // Mostrar mensaje de error
-//                 message.textContent = 'Inicio de sesión fallido. Verifica tus credenciales.';
-//                 message.classList.remove('hidden');
-//             }
-//         })
-//         .catch(error => {
-//             // Mostrar mensaje de error en caso de fallo de la API
-//             message.textContent = 'Ha ocurrido un error. Inténtalo de nuevo más tarde.';
-//             message.classList.remove('hidden');
-//         });
-//     });
-// });
-
 document.addEventListener("DOMContentLoaded", function (e) {
 
   var miForm = document.getElementById('loginForm');
@@ -46,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
     // Obtener los valores de los campos de entrada por su nombre
     const username = formData.get("username");
     const password = formData.get("password");
-  
+
     fetch('/api-login', {
       method: 'POST',
       headers: {
@@ -67,24 +29,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
         console.log(token)
 
         window.location.href = "/dashboard";
-
-        // // Obtén el elemento select por su ID
-        // const select = document.getElementById("co");
-
-        // // Recorre el arreglo de opciones con forEach y crea elementos option
-        // data.forEach(function (opcion) {
-        //   const option = document.createElement("option");
-        //   option.value = opcion.f_id;  // El valor de la opción
-        //   option.text = opcion.f_descripcion;   // El texto visible de la opción
-        //   select.appendChild(option);  // Agrega la opción al select
-        // });
-
-
-
-
       });
-
-
   }
-
 });
